739cfe238048267d31f5028389bf297e286d7d96
valentinadepiero <depierovalentina@gmail.com>
2026-05-18 19:47:17 -0300

    funciones pink noise y sine sweep

--- app/services/pink_noise.py
+++ app/services/pink_noise.py
@@ -3,6 +3,8 @@
 Milestone 1: Generacion de senales.
 """
 import numpy as np
+import soundfile as sf
+import sounddevice as sd
 
 def generar_ruido_rosa(duracion: float, fs: int) -> np.ndarray:
     """
@@ -21,14 +23,33 @@ def generar_ruido_rosa(duracion: float, fs: int) -> np.ndarray:
         Array con la senal de ruido rosa normalizada entre -1 y 1.
     """
     
-    n_muestras = int(duracion * fs)
-    r_blanco = np.random.randn(n_muestras)
-    rap_fou = np.fft.rfft(r_blanco)
+    n_bits = 16;                # numero de bits, es decir numero de generadores
+    n_muestras = int(duracion * fs)  # numero de muestras    
 
-    frec = np.fft.rfftfreq(n_muestras, d=1 / fs)
-    filtro = 1 / np.sqrt(frec + 1e-10)                # Agrega un numero pequeño para no dividir por cero
+    generadores = [np.array(np.random.randn(n_muestras)) for n in range(0, n_bits)] # array de la profundidad de bits elegida con los generadores de ruido.
+    r_rosa = np.array(sum(generadores)) # la primera muestra del ruido rosa va a ser la suma de los generadores 
+    contador = 0 # contador que voy a usar para controlar cuando actualizar los arrays
+    
+    # con un for armo mi senal sumando los generadores en cada muestra
+    for i in range(0,n_muestras):
+        contador += 1 
+        np.append(r_rosa,sum(generadores)) # en cada muestra agrego mi suma de generadores
 
-    r_rosa = np.fft.irfft(rap_fou * filtro)
-    r_rosa = r_rosa / np.max(np.abs(r_rosa))          # Normaliza entre -1 y 1
+        for n in range(0, n_bits): # en la consigna dice que los generadores i tienen que actualizarse cada 2^i (supongo que muestras), asi que aca itero por mi numero de bits (o sea el numero de generadores) y me fijo si la cantidad de muestras que ya sume coincide con 2^i, si lo hace actualizo los generadores
+            if contador == 2 ** n: 
+                generadores[n] = np.random.randn(n_muestras)
+    
+    # este es el codigo de fede que comente por las dudas, si el mio es muy choto podemos volver a este
+    # rap_fou = np.fft.rfft(r_blanco)
+
+    # frec = np.fft.rfftfreq(n_muestras, d=1 / fs)
+    # filtro = 1 / np.sqrt(frec + 1e-10)               
+
+    # r_rosa = np.fft.irfft(rap_fou * filtro)
+    # r_rosa = r_rosa / np.max(np.abs(r_rosa))        
 
     return r_rosa
+
+# descomenten esto para escuchar como suena (tarda un rato asi q paciencia)
+# sd.play(generar_ruido_rosa(1,44100))
+# sd.wait()
--- app/services/sine_sweep.py
+++ app/services/sine_sweep.py
@@ -4,6 +4,7 @@ Milestone 1: Generacion de senales.
 """
 import numpy as np
 import math as ma
+import sounddevice as sd
 
 def generar_sine_sweep(f1: float, f2: float, duracion: float, fs: int) -> tuple[np.ndarray, np.ndarray]:
     """
@@ -26,10 +27,19 @@ def generar_sine_sweep(f1: float, f2: float, duracion: float, fs: int) -> tuple[
         Tupla con (sweep, filtro_inverso), ambos normalizados.
     """
     
-    t = np.linspace(0, duracion, int(duracion * fs))
+    t = np.linspace(0, duracion, int(duracion * fs)) # crea un array de muestras de la duracion que se haya pedido, ubicando cada muestra a una distancia de igual tamano entre ellas. (eso hace el linspace)
     
-    sine_sweep = ma.sin(2 * ma.pi * f1 * duracion * (ma.exp(t * (ma.log(f2 / f1) / duracion) - 1)) / ma.log(f2 / f1))
+    sine_sweep = [ma.sin(2 * ma.pi * f1 * duracion * (ma.exp(n * (ma.log(f2 / f1) / duracion) - 1)) / ma.log(f2 / f1)) for n in t] # aca hago la formula esa que esta en el apunte por cada muestra que necesito y lo meto todo en un array
 
-    filt_inv = (ma.sin(2 * ma.pi * f1 * duracion * (ma.exp((duracion - t) * (ma.log(f2 / f1) / duracion) - 1)) / ma.log(f2 / f1))) / ma.exp(-t * ma.log(f2 / f1) / duracion)
+    filt_inv = [ma.sin(
+                    (2 * ma.pi * f1 * duracion * 
+                        (ma.exp((duracion - n) * ma.log(f2 / f1) / duracion) - 1))
+                            / ma.log(f2 / f1)) 
+                / ma.exp(-n * ma.log(f2 / f1) / duracion) for n in t] 
+    # lo mismo con el filtro, aplico la formula del apunte asignandole un seno a cada muestra
 
-    return sine_sweep * filt_inv
+    return tuple[sine_sweep, filt_inv] # devuelve en una tupla ambas senales
+
+# para escuchar como suena descomenten este codigo y reemplacen el return de aca arriba por return sine_sweep 
+# sd.play(generar_sine_sweep(400,4000,1,44100))
+# sd.wait()
