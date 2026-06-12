883be4fc75856eb85fa70670b427238c9f5be4ce
valentinadepiero <depierovalentina@gmail.com>
2026-06-12 17:20:21 -0300

    vuelvo a una version vieja del sine sweep

--- app/services/sine_sweep.py
+++ app/services/sine_sweep.py
@@ -31,22 +31,33 @@ def generar_sine_sweep(f1: float, f2: float, duracion: float, fs: int) -> tuple[
         Tupla con (sweep, filtro_inverso), ambos normalizados.
     """
 
-
-    if f1 == 0:  # asi no divide por cero
+    if f1 == 0:  # si la frecuencia pedida es 0, se usa un numero muy pequeño en su lugar para no dividir por cero
         f1 += 1e-10
 
+    t = np.linspace(0, duracion, int(duracion * fs)) 
+    # crea un array de muestras de la duracion que se haya pedido, ubicando cada muestra a una distancia de igual tamano entre ellas. (eso hace el linspace)
 
-    n_muestras = int(duracion * fs)
+# t = np.arange(n_muestras) / fs 
 
+    sine_sweep = [ma.sin
+                    (2 * ma.pi * f1 * duracion * 
+                        (ma.exp(n * (ma.log(f2 / f1) / duracion) - 1))
+                    / ma.log(f2 / f1)) 
+                for n in t] 
+    # aca hago la formula esa que esta en el apunte por cada muestra que necesito y lo meto todo en un array
 
-    t = np.arange(n_muestras) / fs
-    k = ma.log(f2 / f1) / duracion
-    o = 2 * ma.pi * f1 / k
+# k = ma.log(f2 / f1) / duracion
+# o = 2 * ma.pi * f1 / k
+# fase = o * (np.exp(k * t) - 1)
+# sine_sweep = np.sin(fase)
+# filt_inv = np.sin(o * (np.exp(k * (duracion - t)) - 1)) * np.exp(-k * t)
 
-
-    fase = o * (np.exp(k * t) - 1)
-    sine_sweep = np.sin(fase)
-    filt_inv = np.sin(o * (np.exp(k * (duracion - t)) - 1)) * np.exp(-k * t)
+    filt_inv = [ma.sin(
+                    (2 * ma.pi * f1 * duracion * 
+                        (ma.exp((duracion - n) * ma.log(f2 / f1) / duracion) - 1))
+                            / ma.log(f2 / f1)) 
+                / ma.exp(-n * ma.log(f2 / f1) / duracion) for n in t] 
+    # lo mismo con el filtro, aplico la formula del apunte asignandole un seno a cada muestra
 
 
     funcion1 = np.asarray(sine_sweep, dtype=np.float64)
@@ -64,10 +75,8 @@ def generar_sine_sweep(f1: float, f2: float, duracion: float, fs: int) -> tuple[
     return funcion1, funcion2
 
 
-
-
 if __name__ == "__main__":
-    sd.play(generar_sine_sweep(400, 4000, 1, 44100)[0])
+    sd.play(generar_sine_sweep(20, 4000, 1, 44100)[0])
     # si ponen [0] hace el sweep normal y si ponen [1] hace el inverso
 
 
