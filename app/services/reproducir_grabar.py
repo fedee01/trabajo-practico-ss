a9e3c94d3a2933057f4ff5750a1c7485f27169ae
fedee01 <fedeegionco@gmail.com>
2026-06-12 18:13:58 -0300

    Validate recording duration against signal length
    Added validation for recording duration to ensure it meets the length of the signal.

--- app/services/reproducir_grabar.py
+++ app/services/reproducir_grabar.py
@@ -25,12 +25,20 @@ def reproducir_y_grabar(signal: np.ndarray, fs: int, duracion_grabacion: float) 
     np.ndarray
         Array con la señal grabada.
     """
-
-    pre_roll = np.zeros(int(0.5 * fs)) # Crea un array de silencio para el pre-roll
-    senal_final = np.concatenate((pre_roll, signal)) # Concatena el pre-roll con la señal original
+    if duracion_grabacion < len(signal) / fs:
+        raise ValueError("La duración de grabación debe ser mayor o igual a la de la señal")
+        
+    pre_roll = np.zeros(int(0.5 * fs))                     # Crea un array de silencio para el pre-roll
+    senal_final = np.concatenate((pre_roll, signal))       # Concatena el pre-roll con la señal original
     
     if not sd.query_devices():
         raise RuntimeError("No se encontraron dispositivos de audio disponibles.") 
+
+    max_abso = np.nanmax(np.abs(senal_final))              # Normaliza al 90%
+    if np.isfinite(max_abso) and max_abso > 0:
+        senal_final = (0.9 * senal_final.astype(np.float32)) / max_abso
+    else:
+        senal_final = senal_final.astype(np.float32)
     
     grabacion = sd.playrec(senal_final, samplerate=fs, channels=signal.shape[1] if signal.ndim > 1 else 1, dtype='float32') # Reproducir y grabar simultáneamente
     sd.wait()  # Esperar a que la reproducción y grabación terminen
