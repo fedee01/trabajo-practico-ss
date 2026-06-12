6a6c823dde07108bd652f6c63e960dbf013d2b1f
fedee01 <fedeegionco@gmail.com>
2026-06-12 19:56:50 -0300

    Refactor reproducir_y_grabar function for clarity

--- app/services/reproducir_grabar.py
+++ app/services/reproducir_grabar.py
@@ -2,16 +2,10 @@
 
 Milestone 1: Generacion de senales.
 """
-
 import sounddevice as sd
 import numpy as np
 
-
-def reproducir_y_grabar(
-    signal: np.ndarray,
-    fs: int,
-    duracion_grabacion: float
-) -> np.ndarray:
+def reproducir_y_grabar(signal: np.ndarray, fs: int, duracion_grabacion: float) -> np.ndarray:
     """
     Reproduce una señal y graba simultáneamente.
 
@@ -31,13 +25,12 @@ def reproducir_y_grabar(
         Array con la señal grabada.
     """
     if duracion_grabacion < len(signal) / fs:
-        raise ValueError("La duración de grabación debe ser mayor o igual a la duración de la señal.")
-
+        raise ValueError("La duración de grabación debe ser mayor o igual a la de la señal.")
+        
     if not sd.query_devices():
-        raise RuntimeError("No se encontraron dispositivos de audio disponibles.")
+        raise RuntimeError("No se encontraron dispositivos de audio.")
 
-    muestras_grabacion = int(duracion_grabacion * fs)
-
+    
     # agrega el pre roll por si la pc tarda
     if signal.ndim == 1:
         pre_roll = np.zeros(int(0.5 * fs))
@@ -45,8 +38,11 @@ def reproducir_y_grabar(
         pre_roll = np.zeros((int(0.5 * fs), signal.shape[1]))
 
     senal_final = np.concatenate((pre_roll, signal))
+
     
     # agrega el padding (ceros al final para que la señal dure lo mismo que la original)
+    muestras_grabacion = int(duracion_grabacion * fs)
+    
     if len(senal_final) < muestras_grabacion:
         if signal.ndim == 1:
             padding = np.zeros(muestras_grabacion - len(senal_final), dtype=senal_final.dtype)
@@ -55,20 +51,18 @@ def reproducir_y_grabar(
                 (muestras_grabacion - len(senal_final), signal.shape[1]), dtype=senal_final.dtype)
 
         senal_final = np.concatenate((senal_final, padding))
-        
     else:
         senal_final = senal_final[:muestras_grabacion]
+
     
     # normaliza
-    max_abso = np.nanmax(np.abs(senal_final))        # nanmax devuelve un maximo pero si la señal esta corrupta no explota todo
+    max_abso = np.nanmax(np.abs(senal_final))    # nanmax devuelve un maximo pero si la señal esta corrupta no explota todo
     if np.isfinite(max_abso) and max_abso > 0:
         senal_final = (0.9 * senal_final.astype(np.float32)) / max_abso
     else:
-        senal_final = senal_final.astype(np.float32)  # si es 0 o la señal se corrompio
+        senal_final = senal_final.astype(np.float32)    # si es 0 o la señal se corrompio
 
     
     grabacion = sd.playrec(senal_final, samplerate=fs, channels=signal.shape[1] if signal.ndim > 1 else 1, dtype="float32")
-
     sd.wait()
-
     return grabacion
