8f17a32274a1e838e7aa289ab1622d510dcb07b8
valentinadepiero <depierovalentina@gmail.com>
2026-06-15 15:06:19 -0300

    nueva branch

--- app/testeando.py
+++ app/testeando.py
@@ -1,4 +1,4 @@
 from services.signal_utils import cargar_audio
 
 resultados = cargar_audio('/home/hola/Documents/trabajo-practico-ss/app/usoesteWAVparatestear.wav')
-print(f"la variable es: {resultados}")
+print(f"{resultados}")
